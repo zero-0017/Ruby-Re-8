8fe09ccb90506756f11217189e1be5b06701e139
zero-0017 <hiromichi.v.0017@gmail.com>
2023-04-30 13:27:37 +0000

    [ruby]9章

---
+++ new.rb
@@ -0,0 +1,26 @@
+class Car
+  def move(direction, distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+
+  def turn(direction)
+    puts "#{direction}に曲がります。"
+  end
+
+  def run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+car=Car.new
+car.move("右",5)
+
+
+class Car
+  def self.turn(distance)
+    puts "#{distance}に曲がります。。"
+  end
+end
+
+Car.turn("右")
